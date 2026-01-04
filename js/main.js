653d19b9a8f6d112ab69633dc90e6cc1f27b2f98
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-04 23:40:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('navMenu');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
 
